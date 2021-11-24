a9167e444d25488c53c35bbbc8d322c724923fd7
QuinnG <quinngilbert12and3@gmail.com>
2021-11-24 18:31:13 -0500

    added exercise06

---
+++ exercise06/src/main/java/Solution06.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Quinn Gilbert
+ */
+import java.util.*;
+
+public class Solution06 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //prompt age
+        System.out.print("What is your current age? ");
+        //input age
+        int age = in.nextInt();
+        //prompt retirement age
+        System.out.print("What age do you want to retire? ");
+        //input retirement age
+        int retirement = in.nextInt();
+        //get current year
+        int current_year = Calendar.getInstance().get(Calendar.YEAR);
+        //print currentYear+retirementAge-currentAge
+        System.out.println("You have "+(retirement-age)+" years left to retire");
+        System.out.println("It's "+current_year+", so you can retire in "+(current_year+retirement-age));
+    }
+}
